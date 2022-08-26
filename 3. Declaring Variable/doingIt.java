9567bd2d29836ee8473be89ce5b4e315e4602c45
itfeelsharsh <itfeelsharsh@gmail.com>
2022-08-26 20:46:23 +0530

    Change

--- 3. Declaring Variable/doingIt.java
+++
@@ -1,9 +0,0 @@
-class doingIt {
-    public static void main (Strings[] s)
-    {
-        float rate;
-        rate = 0.05;
-        double amt$=10000; amt$=rate * amt$;
-        System.out.println("Amount:= " + amt$ + " AUD");
-    }
-}
