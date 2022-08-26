546d6489e352c4e9c9504bc6e1faac016703f1b3
itfeelsharsh <itfeelsharsh@gmail.com>
2022-08-26 21:38:56 +0530

    ArithOperators

---
+++ 1. Fundamentals of Java/ArithOperators.java
@@ -0,0 +1,27 @@
+class ArithOperators{
+    public static void main(String[] args){
+
+        short x = 6;
+        int y = 4;
+        float a = 12.5f;
+        float b = 7.2f;
+
+        System.out.println("");
+        System.out.println("----------Arith Ops----------");
+        System.out.println("X is: " + x);
+        System.out.println("Y is: " + y);
+        System.out.println("X plus Y is: " + (x + y));
+        System.out.println("X minus Y is: " + (x - y));
+        System.out.println("X divided by Y is: " + (x / y));
+        System.out.println("X's remainder when divied by Y is: " + (x % y));
+        System.out.println("");
+        System.out.println("------------------------------");
+        System.out.println("A is: " + a);
+        System.out.println("B is: " + b);
+        System.out.println("A divided by B is: " + (a / b));
+        System.out.println("A's remainder when divied by Y is: " + (a % b));
+        System.out.println("");
+
+
+    }
+}
